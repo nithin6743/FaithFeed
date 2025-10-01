5ddac75faae9cd1a96aed4ef381084d954612b12
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-10-01 10:40:53 +0530

    deleted few files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 export default function App() {
-  return (
-    <div>
-      <img className='bg' src='/Background.jpg' alt='Background' />
-    </div>
-  );
+  return <div>
+    <Header />
+  </div>
 }
 
-function Header() {
-  return;
+function Header(){
+  return <div className="header"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/Background.jpg)`,
+        backgroundSize: "cover", // fills the screen
+        backgroundPosition: "center", // centers the image
+        backgroundRepeat: "no-repeat", // no tiling
+        height: "100vh", // full viewport height
+        width: "100vw", // full viewport width
+        overflow: "hidden", // avoids scrollbars
+      }}>
+        <button className="LogoName">FaithFeed</button>
+        <button className="Date">01/10/2025</button>
+        <button className="Login">Login</button>
+      </div>
 }
