c2958b569de8b941b83c944f6df6e05a33467ef0
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-10-04 11:16:26 +0530

    templating login and signup page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 export default function App() {
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    <div>
-      <Header />
-      <Main />
-      
+    <div className='App'>
+      <Header isLogin={isLogin} setIsLogin={setIsLogin} />
+      <Main isLogin={isLogin} setIsLogin={setIsLogin} />
     </div>
   );
 }
 
-function Header() {
+function Header({ isLogin, setIsLogin }) {
   const today = new Date();
   // console.log(today);
   const formattedDate = today.toLocaleDateString("en-IN", {
@@ -17,33 +20,62 @@ function Header() {
     year: "numeric",
   });
   // console.log(formattedDate);
-  
+  function handleClick() {
+    setIsLogin(!isLogin);
+  }
+
   return (
     <div className='header'>
-      <a className='LogoName' href='#'>
+      <h1 className='LogoName' href='#'>
         FaithFeed
-      </a>
+      </h1>
       <p className='Date'>{formattedDate}</p>
-      <a className='Login' href='https://www.google.com/?zx=1759334362126&no_sw_cr=1'>
-        Login
-      </a>
+      {!isLogin ? (
+        <a className='Login' onClick={handleClick}>
+          Login
+        </a>
+      ) : (
+        <button className='Signup' onClick={handleClick}>
+          Signup
+        </button>
+      )}
     </div>
   );
 }
 
-function Main() {
+function Main({ isLogin, setIsLogin }) {
+
+  function handleEnterButton(e) {
+    e.preventDefault();
+  }
   return (
     <div className='main'>
-      <div className='content'>
-        <h1 className='title'>Start Fresh in Faith</h1>
-        <p className='subtitle'>Explore truth, light, and meaning—one verse at a time.</p>
-        <form className='signupfields'>
-          <input className='signUpInput' type='text' placeholder='Name' />
-          <input className='signUpInput' type='text' placeholder='Email' />
-          <input className='signUpInput' type='text' placeholder='Password' />
-          <button className='OTPbutton' type='submit'>Get OTP</button>
-        </form>
-      </div>
+      {!isLogin ? (
+        <div className='content'>
+          <h1 className='title'>Start Fresh in Faith</h1>
+          <p className='subtitle'>Explore truth, light, and meaning—one verse at a time.</p>
+          <form className='signupfields'>
+            <input className='signUpInput' type='text' placeholder='Name' />
+            <input className='signUpInput' type='text' placeholder='Email' />
+            <input className='signUpInput' type='text' placeholder='Password' />
+            <button className='enterPage' onClick={handleEnterButton}>
+              Get OTP
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className='content'>
+          <h1 className='title'>Welcome Home</h1>
+          <p className='subtitle'>The Word’s been waiting. Your faith story continues here.</p>
+          <form className='signupfields'>
+            <input className='signUpInput' type='text' placeholder='Email' />
+            <input className='signUpInput' type='text' placeholder='Password' />
+            <button className='enterPage' onClick={handleEnterButton}>
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
